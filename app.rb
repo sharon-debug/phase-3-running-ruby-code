6b8a41ccccd60687bba846f0c2b584ce53c26de7
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-10-17 13:52:29 +0300

    done

---
+++ app.rb
@@ -0,0 +1,18 @@
+#app.rb
+
+#puts "Hello world!"
+#puts "Hello world!"
+#puts "Hello world!"
+puts "Hello world!"
+puts "Hello World!"
+
+
+#puts [1, 2, 3]
+
+p [1, 2, 3]
+
+#puts [1, 2, 3].inspect
+
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print "Pass this test, please."
